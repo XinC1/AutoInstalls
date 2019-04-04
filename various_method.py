12c94a8ea663639de52dc7d7ebb2c4b08ed08be5
XinC1 <38888338+XinC1@users.noreply.github.com>
2019-04-04 20:44:56 +0800

    Add files via upload

---
+++ various_method.py
@@ -0,0 +1,22 @@
+a = 5
+b= 10
+my_variable=56
+
+#string_variable = "hello"
+single_quators = 'strings can have single quators'
+
+##print ("hello")
+#print (123)
+
+##
+def my_print_method():
+	print("hello")
+	print("wirld")
+
+my_print_method()
+
+def my_multiply_method(num1,num2):
+	return num1*num2
+
+result= my_multiply_method(5,3)
+print(result)
